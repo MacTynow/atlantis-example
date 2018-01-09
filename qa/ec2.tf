13c6037f97bf1a845316b09b6d3d381743e324dc
MacTynow <charles.martinot@grab.com>
2018-01-09 18:40:20 +0800

    Add test ec2

--- qa/ec2.tf
+++ qa/ec2.tf
@@ -9,3 +9,15 @@ module "ec2_etcd" {
   vpc_security_group_ids = []
   iam_instance_profile   = ""
 }
+
+module "ec2_test" {
+  source        = "../aws/ec2"
+  count         = "1"
+  subnet_id     = ""
+  instance_type = "t2.micro"
+  name          = "instance"
+  key_name      = "key"
+
+  vpc_security_group_ids = []
+  iam_instance_profile   = ""
+}
